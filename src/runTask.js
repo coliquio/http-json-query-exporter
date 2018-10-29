a3d451c12d13ae17e4ba5050306d982d29c2b829
Jens Bissinger <mail@jens-bissinger.de>
2018-10-29 10:45:30 +0100

    Improve logging

--- src/runTask.js
+++ src/runTask.js
@@ -5,7 +5,9 @@ const renderPrometheusMetric = require('./renderPrometheusMetric');
 module.exports = async ({query, transformation, prometheusMetric}) => {
   const data = await queryHttp(query);
   const transformed = await transform(transformation, data);
-  if (typeof transformed !== 'object' || transformed === null) throw new Error(`Transformation failed for ${JSON.stringify(query)} with result ${JSON.stringify(data)}`);
+  if (typeof transformed !== 'object' || transformed === null) {
+    throw new Error(`Transformation failed for ${JSON.stringify(query) .replace(/\\n/g, '')} with result ${JSON.stringify(data) .replace(/\\n/g, '')}`);
+  }
   const renderedPrometheusMetric = renderPrometheusMetric(prometheusMetric, transformed);
   return renderedPrometheusMetric;
 };
