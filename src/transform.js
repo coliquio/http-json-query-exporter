f7724acdbde383569116e867611332a58a9d9143
Jens Bissinger <mail@jens-bissinger.de>
2018-10-29 10:47:14 +0100

    Transform returns empty array if no match

--- src/transform.js
+++ src/transform.js
@@ -3,7 +3,7 @@ const jsonata = require('jsonata');
 const transform = (expression, json) => {
   return new Promise((resolve, reject) => {
     try {
-      resolve(jsonata(expression).evaluate(json));
+      resolve(jsonata(expression).evaluate(json) || []);
     } catch (e) {
       reject(e);
     }
--- test/transformTest.js
+++ test/transformTest.js
@@ -16,4 +16,9 @@ describe('transformTest', () => {
     `, JSON.parse(fs.readFileSync('./test/assets/query-response.json', 'utf8')));
     assert.deepEqual(transformed, JSON.parse(fs.readFileSync('./test/assets/query-response-normalized.json', 'utf8')));
   });
+
+  it('returns empty', async () => {
+    const transformed = await transform(`$.foo.bar`, JSON.parse(fs.readFileSync('./test/assets/query-response.json', 'utf8')));
+    assert.deepEqual(transformed, []);
+  });
 });
