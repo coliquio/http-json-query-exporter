227737546dcd6cae83f348f2b65e25f6a4f59379
Jens Bissinger <mail@jens-bissinger.de>
2020-10-15 19:53:40 +0200

    Fix linter issues

--- test/internalMetricCounterTest.js
+++ test/internalMetricCounterTest.js
@@ -4,44 +4,44 @@ const internalMetricCounter = require('../src/internalMetricCounter');
 describe('internalMetricCounter', () => {
   beforeEach(() => {
     internalMetricCounter.resetState();
-  })
+  });
   
   it('returns value', () => {
-    assert.equal(internalMetricCounter.get({foo: 1}), 0)
+    assert.equal(internalMetricCounter.get({foo: 1}), 0);
   });
 
   it('increments and returns value', () => {
-    internalMetricCounter.increment({foo: 1})
-    assert.equal(internalMetricCounter.get({foo: 1}), 1)
+    internalMetricCounter.increment({foo: 1});
+    assert.equal(internalMetricCounter.get({foo: 1}), 1);
   });
 
   it('sets, gets and increments value', () => {
-    internalMetricCounter.set({foo: 1}, 10)
-    internalMetricCounter.set({foo: 2}, 20)
-    internalMetricCounter.set({foo: 1, bar: 1}, 30)
-    internalMetricCounter.set({foo: 1, baz: 1}, 40)
-    assert.equal(internalMetricCounter.get({foo: 1}), 10)
-    assert.equal(internalMetricCounter.get({foo: 2}), 20)
-    assert.equal(internalMetricCounter.get({foo: 1, bar: 1}), 30)
-    assert.equal(internalMetricCounter.get({foo: 1, baz: 1}), 40)
-    internalMetricCounter.increment({foo: 1})
-    internalMetricCounter.increment({foo: 2})
-    internalMetricCounter.increment({foo: 1, bar: 1})
-    internalMetricCounter.increment({foo: 1, baz: 1})
-    assert.equal(internalMetricCounter.get({foo: 1}), 11)
-    assert.equal(internalMetricCounter.get({foo: 2}), 21)
-    assert.equal(internalMetricCounter.get({foo: 1, bar: 1}), 31)
-    assert.equal(internalMetricCounter.get({foo: 1, baz: 1}), 41)
+    internalMetricCounter.set({foo: 1}, 10);
+    internalMetricCounter.set({foo: 2}, 20);
+    internalMetricCounter.set({foo: 1, bar: 1}, 30);
+    internalMetricCounter.set({foo: 1, baz: 1}, 40);
+    assert.equal(internalMetricCounter.get({foo: 1}), 10);
+    assert.equal(internalMetricCounter.get({foo: 2}), 20);
+    assert.equal(internalMetricCounter.get({foo: 1, bar: 1}), 30);
+    assert.equal(internalMetricCounter.get({foo: 1, baz: 1}), 40);
+    internalMetricCounter.increment({foo: 1});
+    internalMetricCounter.increment({foo: 2});
+    internalMetricCounter.increment({foo: 1, bar: 1});
+    internalMetricCounter.increment({foo: 1, baz: 1});
+    assert.equal(internalMetricCounter.get({foo: 1}), 11);
+    assert.equal(internalMetricCounter.get({foo: 2}), 21);
+    assert.equal(internalMetricCounter.get({foo: 1, bar: 1}), 31);
+    assert.equal(internalMetricCounter.get({foo: 1, baz: 1}), 41);
   });
 
   it('sets and gets labels', () => {
-    internalMetricCounter.increment({foo: 1})
-    internalMetricCounter.increment({foo: 2})
-    internalMetricCounter.set({foo: 1, bar: 1}, 5)
+    internalMetricCounter.increment({foo: 1});
+    internalMetricCounter.increment({foo: 2});
+    internalMetricCounter.set({foo: 1, bar: 1}, 5);
     assert.deepEqual(internalMetricCounter.getLabels(), [
-      {foo: "1"},
-      {foo: "2"},
-      {foo: "1", bar: "1"}
-    ])
+      {foo: '1'},
+      {foo: '2'},
+      {foo: '1', bar: '1'}
+    ]);
   });
 });
