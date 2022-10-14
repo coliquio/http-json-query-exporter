545dc5249cdd0a2e960a21180b7a80bf1cdb8813
Jens Bissinger <mail@jens-bissinger.de>
2022-10-14 10:29:28 +0200

    Refactor queryHttp

--- src/queryHttp.js
+++ src/queryHttp.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
 
 module.exports = async (options) => {
-  return axios.request(Object.assign({
-    timeout: 2000
-  }, options))
-    .then(res => {
-      return res.data;
-    })
-    .catch((err) => {
-      throw new Error(`httpQuery failed ${err.message} with options=${JSON.stringify(options).replace(/\\n/g, '')}`);
-    });
+  try {
+    const { data } = await axios.request(Object.assign({
+      timeout: 2000
+    }, options));
+    return data;
+  } catch (err) {
+    throw new Error(`httpQuery failed ${err.message} with options=${JSON.stringify(options).replace(/\\n/g, '')}`);
+  }
 };
